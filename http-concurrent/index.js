1abd90c0d97034931d56e1d0a26392a174c3f153
wanyuqing <wanyuqing@meituan.com>
2019-12-16 23:19:05 +0800

    http-concurrent

--- http-concurrent/index.js
+++ http-concurrent/index.js
@@ -4,31 +4,34 @@
 const LIST = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 const AJAX_LIST = [];
 const LIMIT = 2;
-
 LIST.forEach(num => {
     AJAX_LIST.push(() => new Promise((resolve, reject) => {
+        console.log('发送请求, 序号: ', num);
         setTimeout(() => {
-            console.log('ajax index: ', num);
+            console.log('response: ', num);
             resolve(num)
-        }, 0)
+        }, Math.random() * 1000)
     }))
 });
 
 
 const limitAsyncHttp = (AJAX_LIST, LIMIT) => {
-    const ASYNC_LIST = [];
-    while(LIMIT--) {
-        ASYNC_LIST.push(AJAX_LIST.shift());
-    };
-    while(ASYNC_LIST.length) {
-        ASYNC_LIST.shift()().then(num => {
-            console.log(num);
-        })
+    let ASYNC_LIST = [].concat(AJAX_LIST);
+    let limit = LIMIT;
+    const handler = () => {
+        if(ASYNC_LIST.length) {
+            ASYNC_LIST.shift()().then(num => {
+                handler();
+            })
+        } else {
+            return;
+        }
+
+    }
+    while(limit--) {
+        handler();
     }
 }
 
 limitAsyncHttp(AJAX_LIST, LIMIT);
-
-
-// Promise.all(AJAX_LIST);
 
